2b3c268e2d09bb6264c64fdaf32b49b65ceb3c2c
Stefan Chao <Stefan_Chao@gmail.com>
2014-11-30 13:40:56 +0000

    done

--- Lab06.java
+++ Lab06.java
@@ -2,8 +2,8 @@
 *
 * Description of the program goes here  // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author <Stefan Chao>
+* @version <25/nove/2014>
 */
 
    import edu.fcps.karel2.Display;
@@ -29,31 +29,76 @@
        public static void task_01()	
       { //go to the end of the row of beepers
          Robot temp = new Robot(1, 1, Display.EAST, 0);
+         while(temp.nextToABeeper()){
+        	 temp.move();
+         }
       
       }
        public static void task_02()	
       { //go to the beeper
          Robot temp = new Robot(1, 2, Display.EAST, 0);
+         while(!temp.nextToABeeper()){
+        	 temp.move();
+         }
       	
       }
        public static void task_03()	
       { //go to the wall
          Robot temp = new Robot(1, 3, Display.EAST, 0);
+         while(temp.frontIsClear()){
+        	 temp.move();
+         }
        
       }
        public static void task_04()	
       { //go to the wall, pick up all the beepers (max one per pile)
          Robot temp = new Robot(1, 4, Display.EAST, 0);
+         if(temp.nextToABeeper()){
+        	 temp.pickBeeper();
+         }
+         while(temp.frontIsClear()){
+        	 temp.move();
+        	 while(temp.nextToABeeper()){
+        		 temp.pickBeeper();
+        	 }
+         }
       
       }
        public static void task_05()	
       { //go to the wall, pick up all the beepers
          Robot temp = new Robot(1, 5, Display.EAST, 0);
-      
+         
+         if(temp.nextToABeeper()){
+        	 temp.pickBeeper();
+         }
+         while(temp.frontIsClear()){
+        	 temp.move();
+        	 while(temp.nextToABeeper()){
+        		 temp.pickBeeper();
+        	 }
+         }
       }
        public static void task_06()
       { //go to the end of the row of beepers, there is one gap
          Robot temp = new Robot(1, 6, Display.EAST, 0);
-      
+         while (temp.nextToABeeper() == true)
+ 			temp.move();
+ 			while (temp.nextToABeeper() == false)
+ 			temp.move();
+ 			while(temp.nextToABeeper() == true)
+ 			temp.move(); 
+ 			temp.turnLeft();
+ 			temp.turnLeft();
+ 			if (temp.frontIsClear()==true)
+ 			{
+ 				temp.move();
+ 			}   
+ 			temp.turnLeft();
+ 			temp.turnLeft();
+ 		
+         
+         
       }
-   }
+         
+      }
+   
